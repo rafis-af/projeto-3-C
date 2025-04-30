da3c85a9caf4565d9e515bd361189a488ac498a4
rafis-af <rafa.ferreira0301@gmail.com>
2025-04-30 16:05:08 -0300

    Criando a função ler palavras aleatórias

--- funcoes.c
+++ funcoes.c
@@ -5,3 +5,30 @@
 #include <ctype.h>
 #include "funcoes.h"
 
+void lerPalavraAleatoria(char *palavra, char *dica){
+
+    FILE *fp = open("palavras-sortidas.txt", "r");
+    if (fp == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+
+    char linhas[TAM_MAX][TAM_PALAVRA + TAM_DICA];
+    int total = 0;
+    
+    while (fgets(linhas[total], sizeof(linhas[total]), fp)) {
+
+        if(strchr(linhas[total], ':')){
+            total++;
+        }
+        fclose(fp);
+
+        srand(time(NULL));
+        int sorteio = rand() % total;
+
+        char *token = strtok(linhas[sorteio], ":");
+        strcpy(palavra, token);
+        token = strtok(NULL, ":");
+        strcpy(dica, token);
+    }
+}
---
+++ funcoes.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define TAM_MAX 100
+#define TAM_PALAVRA 30
+#define TAM_DICA 100
+
+void lerPalavraAleatoria(char *palavra, char *dica);
+
+
+
+
+
+#endif
